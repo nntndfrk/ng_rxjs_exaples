fa0cba9a323195a75564fd41497cd533611a1003
nntndfrk <nntndfrk@gmail.com>
2018-07-18 19:44:36 +0300

    merge first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {from, merge} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h4>RxJS Examples</h4>
-  <p></p>
+    <h4>RxJS Examples</h4>
+    <p>merge()</p>
   `,
 })
 export class AppComponent implements OnInit {
 
   ngOnInit() {
+    const s1$ = from(['Hello', 'world']);
+    const s2$ = from(['Lorem', 'ipsum']);
+
+    merge(s1$, s2$)
+      .subscribe(data => console.log(data));
   }
 
 }
