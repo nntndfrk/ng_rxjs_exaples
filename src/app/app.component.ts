96d0510d296bf8abeeaf29607ae6e75c15bddeb4
nntndfrk <nntndfrk@gmail.com>
2018-07-18 19:38:02 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h4>RxJS Examples</h4>
+  <p></p>
+  `,
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
 }
