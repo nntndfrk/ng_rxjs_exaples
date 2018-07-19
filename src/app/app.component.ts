fcdcb5fc6bd1bb6b1ae5e0a2baaf2a853c13cb4c
nntndfrk <nntndfrk@gmail.com>
2018-07-19 11:46:24 +0300

    AsyncSubject first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,30 @@
 import {Component, OnInit} from '@angular/core';
-import {interval, Subject} from 'rxjs';
+import {AsyncSubject, interval} from 'rxjs';
 import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   template: `
     <h4>RxJS Examples</h4>
-    <p>hot Observable</p>
+    <p>AsyncSubject</p>
   `,
 })
 export class AppComponent implements OnInit {
 
   ngOnInit() {
-    const source$ = new Subject();
+    const source$ = new AsyncSubject();
 
     interval(500)
       .pipe(take(10))
-      .subscribe(x => {
-        source$.next(x);
-      });
+      .subscribe(
+        x => {
+          source$.next(x);
+        },
+        null,
+        () => {
+          source$.complete();
+        }
+      );
 
     source$.subscribe(x => console.log('from first subscription - ', x));
     setTimeout(() => {
