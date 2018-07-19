71d42248bbacd23bc0e0f3548dc1c73957f7c0c7
nntndfrk <nntndfrk@gmail.com>
2018-07-19 11:54:10 +0300

    mergeAll click text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {map, mergeAll, take} from 'rxjs/operators';
   template: `
     <h4>RxJS Examples</h4>
     <p>mergeAll()</p>
+    <p style="color: crimson">click!</p>
   `,
 })
 export class AppComponent implements OnInit {
