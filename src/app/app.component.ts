623c335b93e0f8c9bfe206384620121f876bb089
nntndfrk <nntndfrk@gmail.com>
2018-07-19 09:51:56 +0300

    concatMap first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {interval} from 'rxjs';
-import {map, mergeMap, take} from 'rxjs/operators';
+import {concatMap, map, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   template: `
     <h4>RxJS Examples</h4>
-    <p>mergeMap()</p>
+    <p>concatMap()</p>
   `,
 })
 export class AppComponent implements OnInit {
@@ -15,13 +15,7 @@ export class AppComponent implements OnInit {
     const s1$ = interval(1000).pipe(take(3));
     const s2$ = interval(500).pipe(take(3));
 
-    // s1$.subscribe(s1Value => {
-    //   s2$.subscribe(s2Value => {
-    //     console.log(`s1Value - ${s1Value} : s2Value - ${s2Value}`);
-    //   });
-    // });
-
-    s1$.pipe(mergeMap((s1Value) => {
+    s1$.pipe(concatMap((s1Value) => {
           return s2$.pipe(map(s2Value => `s1Value - ${s1Value} : s2Value - ${s2Value}`));
         }))
       .subscribe(x => console.log(x));
